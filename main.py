b165d61ed19d60e35472ff335fb35f31ce3e701e
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-10-14 10:49:00 -0400

    ajout du projet

---
+++ main.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load the model
+model = tf.keras.models.load_model('phishing_detection.keras')
+
+test_string_positive=[
+    "Your account has been compromised. Please click on the link to reset your password.",
+    "You have won a free trip to Hawaii! Click here to claim your prize.",
+    "Your bank account has been locked. Please click on the link to unlock it.",
+    "Your account has been suspended. Please click on the link to reactivate it.",
+    "Your email account has been hacked. Please click on the link to secure it.",
+    "You have been selected for a job interview. Click here to confirm your attendance.",
+    "Your credit card has been charged for an unauthorized purchase. Click here to dispute the charge.",
+    "Your subscription has expired. Please click on the link to renew it.",
+    "You have received a new voicemail message. Click here to listen to it.",
+    "Your account has been flagged for suspicious activity. Please click on the link to verify your identity."
+]
+
+test_string_negative=[
+    "I'm looking forward to seeing you tomorrow.",
+    "The weather is beautiful today.",
+    "I'm sorry, I can't make it to the meeting.",
+    "I'm going to the gym after work.",
+    "What time is the movie tonight?",
+    "I'm running late. Can we reschedule?",
+    "I'm excited to try the new restaurant in town.",
+    "I'm going to the park this weekend.",
+    "I'm taking a break from work next week.",
+    "What are your plans for the weekend?"
+]
+
+
+def predict(predict_msg):
+    token = Tokenizer()
+    token.fit_on_texts(predict_msg)
+    padding_type='post'
+    new_seq = token.texts_to_sequences(predict_msg)
+    padded = pad_sequences(new_seq, maxlen =50,
+                      padding = padding_type,
+                      truncating='post')
+    return (model.predict(padded))
+
+print("Positive examples:")
+print(predict(test_string_positive))
+print("\n\nNegative examples:")
+print(predict(test_string_negative))
---
+++ training.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+data = pd.read_csv('Emails.csv')
+
+ham_msg = data[data.Category =='ham']
+spam_msg = data[data.Category=='spam']
+ham_msg=ham_msg.sample(n=len(spam_msg),random_state=42)
+balanced_data = pd.concat([ham_msg,spam_msg])
+balanced_data['label']=balanced_data['Category'].map({'ham':0,'spam':1})
+
+train_msg, test_msg, train_labels, test_labels =train_test_split(balanced_data['Message'],balanced_data['label'],test_size=0.2,random_state=434)
+
+vocab_size=500
+oov_tok='<OOV>'
+max_len=50
+
+#preprocessing making tokens out of text
+token=Tokenizer(num_words=vocab_size,oov_token=oov_tok)
+token.fit_on_texts(train_msg)
+
+padding_type='post'
+truncate_type='post'
+Trainning_seq=token.texts_to_sequences(train_msg)
+Trainning_pad=pad_sequences(Trainning_seq,maxlen=50,padding=padding_type,truncating=truncate_type)
+
+Testing_seq=token.texts_to_sequences(test_msg)
+Testing_pad=pad_sequences(Testing_seq,maxlen=50,padding=padding_type,truncating=truncate_type)
+
+model=tf.keras.models.Sequential([tf.keras.layers.Embedding(vocab_size,16,input_length=50),
+                                  tf.keras.layers.GlobalAveragePooling1D(),
+                                  tf.keras.layers.Dense(32,activation='relu'),
+                                  tf.keras.layers.Dropout(0.3),
+                                  tf.keras.layers.Dense(1,activation='sigmoid')])
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy'],optimizer='adam')
+
+epoch=30
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+history=model.fit(Trainning_pad, train_labels ,validation_data=(Testing_pad, test_labels),epochs=epoch,callbacks=[early_stop],verbose=2)
+
+model.evaluate(Testing_pad, test_labels)
+model.save('phishing_detection.keras')
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+
+plt.show(block=True)
