d5a52a45d43b6c3e98ce38c543e9fc84caa332ef
Christophe Simard <christophe@mail.com>
2023-10-14 13:59:46 -0400

    Debute with Grafana in backend

--- api/api.py
+++ api/api.py
@@ -18,6 +18,51 @@ async def root(request: PredictRequest):
     print(result.tolist())
     return (result.tolist())
 
+@app.get("/metrics/total_queries")
+async def total_queries():
+    return { "data": [{
+        "value": 100,
+        "name": "Total queries",
+        "time": "2021-10-20T12:00:00Z",
+    }, {
+        "value": 200,
+        "name": "Total queries",
+        "time": "2021-10-20T12:01:00Z",
+    }, {
+        "value": 300,
+        "name": "Total queries",
+        "time": "2021-10-20T12:02:00Z",
+    }]}
+
 @app.get("/metrics/number_blacklist")
 async def number_blacklist():
-    return { "number" : 10}
+    return { "data": [{
+        "value": 100,
+        "name": "Number of blacklisted websites",
+        "time": "2021-10-20T12:00:00Z",
+    }, {
+        "value": 200,
+        "name": "Number of blacklisted websites",
+        "time": "2021-10-20T12:01:00Z",
+    }, {
+        "value": 300,
+        "name": "Number of blacklisted websites",
+        "time": "2021-10-20T12:02:00Z",
+    }]}
+
+@app.get("/metrics/pourcentage_phishing")
+async def pourcentage_phishing():
+    return { "data": [{
+        "value": 0.1,
+        "name": "Pourcentage of phishing websites",
+        "time": "2021-10-20T12:00:00Z",
+    }, {
+        "value": 0.2,
+        "name": "Pourcentage of phishing websites",
+        "time": "2021-10-20T12:01:00Z",
+    }, {
+        "value": 1.18,
+        "name": "Pourcentage of phishing websites",
+        "time": "2021-10-20T12:02:00Z",
+    }]}
+
